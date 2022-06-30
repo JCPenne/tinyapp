8aed65571188a0b044411fa1f1b39550df5fb442
JCPenne <jackcpalmer@icloud.com>
2022-06-30 15:24:01 -0700

    add unit tests for userChecker function

---
+++ test/helper.test.js
@@ -0,0 +1,29 @@
+const { assert } = require('chai');
+
+const { userChecker } = require('../helpers.js');
+
+const testUsers = {
+  userRandomID: {
+    id: 'userRandomID',
+    email: 'user@example.com',
+    password: 'purple-monkey-dinosaur',
+  },
+  user2RandomID: {
+    id: 'user2RandomID',
+    email: 'user2@example.com',
+    password: 'dishwasher-funk',
+  },
+};
+
+describe('getUserByEmail', function () {
+  it('should return a user with valid email', () => {
+    const user = userChecker(testUsers, 'email', 'user@example.com').user;
+    const expectedUserID = 'userRandomID';
+    assert.strictEqual(user, expectedUserID);
+  });
+  it(`should return undefined with invalid email`, () => {
+    const user = userChecker(testUsers, 'email', 'user3@example.com').user;
+    const expectedUserID = undefined;
+    assert.strictEqual(user, expectedUserID);
+  });
+});
